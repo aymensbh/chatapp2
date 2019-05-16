1d2ed5afaccff97df4520e730ab1b311471124e1
aymensbh <abdelkadersebihi19@gmail.com>
2019-05-16 20:03:08 +0100

    Add All Files

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -7,19 +7,45 @@ import 'package:flutter/material.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
 import 'package:line_icons/line_icons.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   PageController _controller;
+  Color color0 = Colors.indigoAccent,
+      color1 = Colors.black,
+      color2 = Colors.black,
+      color3 = Colors.black;
+
+  void change(int index) {
+    color0 = Colors.black;
+    color1 = Colors.black;
+    color2 = Colors.black;
+    color3 = Colors.black;
+
+    switch (index) {
+      case 0:
+      color0=Colors.indigoAccent;
+        break;
+      case 1:
+      color2=Colors.indigoAccent;
+        break;
+      case 2:
+      color2=Colors.indigoAccent;
+        break;
+      case 3:
+      color3=Colors.indigoAccent;
+        break;
+    }
+
+    
+  }
 
   @override
   void initState() {
-    _controller=new PageController(initialPage: 0);
+    _controller = new PageController(initialPage: 0);
     super.initState();
   }
 
@@ -28,18 +54,18 @@ class _HomePageState extends State<HomePage> {
     _controller.dispose();
     super.dispose();
   }
-  
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: firebaseAuth.currentUser(),
-      builder: (context,snapshot){
+      builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Container(
               child: Scaffold(
             appBar: AppBar(
-              title: Text(                          //we can work with this later
+              title: Text(
+                //we can work with this later
                 "Loading..",
               ),
               elevation: 0,
@@ -52,9 +78,12 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ));
-        }else{
+        } else {
           return Scaffold(
             body: PageView(
+              onPageChanged: (index){
+                change(index);
+              },
               controller: _controller,
               children: <Widget>[
                 Home(),
@@ -62,63 +91,67 @@ class _HomePageState extends State<HomePage> {
                 Profile(),
                 Settings(),
               ],
-
             ),
             bottomNavigationBar: Container(
               color: Colors.white,
-              height: MediaQuery.of(context).size.height/10,
+              height: MediaQuery.of(context).size.height / 10,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
                   IconButton(
-                    icon: Icon(LineIcons.home),
-                    onPressed: (){
-                      _controller.animateToPage(
-                      0,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.ease,
-                      );
-                    } 
-                  ),
+                      icon: Icon(
+                        LineIcons.home,
+                        color: color0,
+                      ),
+                      onPressed: () {
+                        _controller.animateToPage(
+                          0,
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.ease,
+                        );
+                      }),
                   IconButton(
-                    icon: Icon(LineIcons.search),
-                    onPressed: (){
-                      _controller.animateToPage(
-                      1,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.ease,
-                      );
-                    } 
-                  ),
+                      icon: Icon(
+                        LineIcons.search,
+                        color: color1,
+                      ),
+                      onPressed: () {
+                        _controller.animateToPage(
+                          1,
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.ease,
+                        );
+                      }),
                   IconButton(
-                    icon: Icon(LineIcons.user),
-                    onPressed: (){
-                      _controller.animateToPage(
-                      2,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.ease,
-                      );
-                    } 
-                  ),
+                      icon: Icon(
+                        LineIcons.user,
+                        color: color2,
+                      ),
+                      onPressed: () {
+                        _controller.animateToPage(
+                          2,
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.ease,
+                        );
+                      }),
                   IconButton(
-                    icon: Icon(LineIcons.cogs),
-                    onPressed: (){
-                      _controller.animateToPage(
-                      3,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.ease,
-                      );
-                    } 
-                  ),
-
+                      icon: Icon(
+                        LineIcons.cogs,
+                        color: color3,
+                      ),
+                      onPressed: () {
+                        _controller.animateToPage(
+                          3,
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.ease,
+                        );
+                      }),
                 ],
               ),
             ),
           );
-
         }
       },
-      
     );
   }
 }
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -9,7 +9,10 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      color: Colors.white,
+      child: Center(
+        child: Text("Settings"),
+      ),
     );
   }
 }
